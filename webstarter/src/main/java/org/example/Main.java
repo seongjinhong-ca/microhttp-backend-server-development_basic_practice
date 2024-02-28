971fba8261dad11ade1744cab330be3c5c4dfab7
seongjin <shawn95981@gmail.com>
2024-02-28 18:44:22 -0500

    able to refactor response message from http request endpoints, and created new handler for dealing accepting end points with 200 OK status which was separated from 400 Not found error and 500 server error.

--- webstarter/src/main/java/org/example/Main.java
+++ webstarter/src/main/java/org/example/Main.java
@@ -18,16 +18,79 @@ package org.example;
 //
 // <DATA>
 
+import dev.mccue.microhttp.handler.IntoResponse;
+import dev.mccue.reasonphrase.ReasonPhrase;
 import org.microhttp.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-class NewHandler implements Handler{
+class TextResponse implements IntoResponse {
+    private final int status;
+    private final List<Header> headers;
+    private final String body;
+
+    public TextResponse(int status, List<Header> headers, String body) {
+        this.status = status;
+        var copied = new ArrayList<>(headers);
+        copied.add(new Header("Content-Type", "text/plain; charset=utf-8"));
+        this.headers = copied;
+        this.body = body;
+    }
+
+    public TextResponse(String body) {
+        this(200, List.of(), body);
+    }
 
     @Override
-    public void handle(Request request, Consumer<Response> consumer) {
+    public Response intoResponse() {
+        return new Response(
+                this.status,
+                ReasonPhrase.forStatus(this.status),
+                headers,
+                body.getBytes()
+        );
+    }
+}
+class NewHandler implements dev.mccue.microhttp.handler.Handler {
 
+    @Override
+    public IntoResponse handle(Request request) throws Exception {
+        if(request.method().equals("GET") && request.uri().equals("/finding")){
+            var response = new Response(
+                    200,
+                    "OK",
+                    List.of(
+                            new Header("content-Type", "text/html")
+                    ),
+                    "finding...".getBytes()
+            );
+            return() ->response;
+        } else if(request.method().equals("POST") && request.uri().equals("/search")){
+            var response = new Response(
+                    200,
+                    "OK",
+                    List.of(
+                            new Header("Content-Type", "text/html"),
+                            new Header("User-Agent" , "Chrome v1.2.3")
+                    ),
+                    "Searching...".getBytes()
+            );
+            return() ->response;
+        } else if(request.method().equals("GET") && request.uri().equals("/hello")){
+            var response = new Response(
+                    200,
+                    "OK",
+                    List.of(
+                            new Header("content-Type", "text/html")
+                    ),
+                    "hello...".getBytes()
+            );
+            return() ->response;
+        }else{
+            return null;
+        }
     }
 }
 class BasicHandler implements Handler{
@@ -35,83 +98,6 @@ class BasicHandler implements Handler{
     @Override
     public void handle(Request request, Consumer<Response> consumer) {
         Thread.startVirtualThread(()->{
-            try{
-                if(request.method().equals("GET") && request.uri().equals("/finding")){
-                    var response = new Response(
-                            200,
-                            "OK",
-                            List.of(
-                                    new Header("content-Type", "text/html")
-                            ),
-                            "finding...".getBytes()
-                    );
-                    consumer.accept(response);
-                } else if(request.method().equals("POST") && request.uri().equals("/search")){
-                    var response = new Response(
-                            200,
-                            "OK",
-                            List.of(
-                                    new Header("Content-Type", "text/html"),
-                                    new Header("User-Agent" , "Chrome v1.2.3")
-                            ),
-                            "Searching...".getBytes()
-                    );
-                    consumer.accept(response);
-                } else if(request.method().equals("GET") && request.uri().equals("/hello")){
-
-                    /*
-                    *
-                    * {
-                    * Key: value
-                    *
-                    * {GET, "/hello"}
-                    *   :
-                    * ()->{
-                    * var response = new Response(
-                            200,
-                            "OK",
-                            List.of(
-                                    new Header("content-Type", "text/html")
-                            ),
-                            "hello...".getBytes()
-                    );
-                    * consumer.accept(response);
-                    * } ()
-                    *
-                    *
-                    * }
-                    * */
-                    var response = new Response(
-                            200,
-                            "OK",
-                            List.of(
-                                    new Header("content-Type", "text/html")
-                            ),
-                            "hello...".getBytes()
-                    );
-                    consumer.accept(response);
-                }else{
-                    var response = new Response(
-                            404,
-                            "Not Found",
-                            List.of(
-                                    new Header("Content-Type", "text/html")
-                            ),
-                            "Not Found Error".getBytes()
-                    );
-                    consumer.accept(response);
-                }
-            } catch (Exception e){
-                var response = new Response(
-                        500,
-                        "Internal Server",
-                        List.of(
-                                new Header("Content-Type", "text/html")
-                        ),
-                        "Internal Server Error!".getBytes()
-                );
-                consumer.accept(response);
-            }
         });
     }
 };
@@ -119,21 +105,45 @@ class BasicHandler implements Handler{
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        var handler = new BasicHandler();
-//
-//        var options = Options.builder()
-//                .withPort(5523)
-//                .build();
-//
-//        var eventLoop = new EventLoop(options, handler);
-//
-//        eventLoop.start();
-//        eventLoop.join();
 
-        var handler = new BasicHandler();
+        var notFound = new Response(
+                404,
+                "Not Found",
+                List.of(
+                        new Header("Content-Type", "text/html")
+                ),
+                "Not Found Error".getBytes()
+        );
+
+        var internalServer = new Response(
+                500,
+                "Internal Server",
+                List.of(
+                        new Header("Content-Type", "text/html")
+                ),
+                "Internal Server Error!".getBytes()
+        );
+
+        var handlers = List.of(
+                new NewHandler()
+        );
 
         var options = Options.builder().withPort(5523).build();
-        EventLoop eventLoop = new EventLoop(options, handler);
+        EventLoop eventLoop = new EventLoop(options, ((request, consumer) -> {
+            try{
+                for(var handler: handlers){
+                    var intoResponse =handler.handle(request);
+                    if(intoResponse != null){
+                        consumer.accept(intoResponse.intoResponse());
+                        return;
+                    }
+                }
+                consumer.accept(notFound);// 400 not found error
+            }catch(Exception e){
+                e.printStackTrace();
+                consumer.accept(internalServer); // 500 server error
+            }
+        }));
 
         eventLoop.start();
         eventLoop.join(); // don't stop
